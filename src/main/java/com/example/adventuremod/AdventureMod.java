167b95673cc91b6b5413808e61f756cafc1c0271
Adam Sidor <sidoadsi1@gmail.com>
2025-01-24 21:13:29 +0100

    initial

---
+++ src/main/java/com/example/adventuremod/AdventureMod.java
@@ -0,0 +1,12 @@
+package com.example.adventuremod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdventureMod.MODID)
+public class AdventureMod {
+    public static final String MODID = "adventuremod";
+
+    public AdventureMod() {
+        System.out.println("Witaj w Pierwszym Modzie!");
+    }
+}
