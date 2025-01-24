427c3688eccd5854fec5c467d9e6087cec218d30
Adam Sidor <sidoadsi1@gmail.com>
2025-01-24 22:41:28 +0100

    Czekam na obrazek

--- src/main/java/com/example/adventuremod/AdventureMod.java
+++ src/main/java/com/example/adventuremod/AdventureMod.java
@@ -1,12 +1,27 @@
 package com.example.adventuremod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(AdventureMod.MODID)
 public class AdventureMod {
     public static final String MODID = "adventuremod";
 
+
     public AdventureMod() {
-        System.out.println("Witaj w Pierwszym Modzie!");
+        IEventBus modEventBus = MinecraftForge.EVENT_BUS;
+        modEventBus.addListener(this::commonSetup);
+
     }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+    }
+
 }
