224d9365d5ff3fd1e749ddf13200e8051c2d905c
Adam Sidor <sidoadsi1@gmail.com>
2025-01-25 21:39:37 +0100

    Blok dodany ale jeszcze błędy

--- src/main/java/com/example/adventuremod/blocks/NewBlocks.java
+++ src/main/java/com/example/adventuremod/blocks/NewBlocks.java
@@ -3,21 +3,21 @@ package com.example.adventuremod.blocks;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-import com.example.adventuremod.AdventureMod;
 
 public class NewBlocks {
     // Tworzymy DeferredRegister dla bloków
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registry.BLOCK_REGISTRY, AdventureMod.MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registry.BLOCK_REGISTRY, "adventuremod");
 
     // Rejestracja Alembica
     public static final RegistryObject<Block> ALEMBIC = BLOCKS.register("alembic",
-            () -> new AlembicBlock(BlockBehaviour.Properties.of(net.minecraft.world.level.material.Material.METAL)));
+            () -> new AlembicBlock(BlockBehaviour.Properties.of(Material.METAL)));
 
     // Funkcja rejestrująca bloki
-    public static void register(IEventBus modEventBus) {
+    /* static void register(IEventBus modEventBus) {
         BLOCKS.register(modEventBus);
-    }
+    }*/
 }
